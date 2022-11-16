84221d171f42e0f730ce82fe6db13d5716571ccd
Milan-PC <Milan-PC@DESKTOP-F3JFC1C>
2022-11-16 15:07:06 +0100

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Order by size from big to small
+            int[] pole = { 1, 80, 5, 2, 7, 80, 13, 20 };
+            int[] poleOrdered = new int[pole.Length];
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                // sets first number in first pos
+                if (i == 0)
+                {
+                    poleOrdered.SetValue(pole[i], i);
+                    continue;
+                }
+
+                var currentVal = pole[i];
+                if (poleOrdered[i - 1] > currentVal)
+                {
+                    poleOrdered.SetValue(currentVal, i);
+                }
+                else
+                {
+                    for (int j = 1; j < i + 1; j++)
+                    {
+                        int val = poleOrdered[i - j];
+                        if (val < currentVal)
+                        {
+                            poleOrdered = MoveDownByOne(i - j, poleOrdered); //moves number down by one 
+                            poleOrdered.SetValue(currentVal, i - j);
+                            if (i - j != 0)
+                                continue;
+                        }
+
+                        //for same numbers
+                        if (val == currentVal)
+                        {
+                            poleOrdered.SetValue(currentVal, i - j + 1);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            foreach (var item in poleOrdered)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] MoveDownByOne(int startPos, int[] pole)
+        {
+            pole.SetValue(pole[startPos], startPos + 1);
+
+            return pole;
+        }
+    }
+}
